5d49c29bd95b2dc676d658fef1710f21457334ef
Valentin <43043885+itsmrval@users.noreply.github.com>
2024-08-06 13:18:56 +0200

    Update uninstall.sh

--- kubernetes/uninstall.sh
+++ kubernetes/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 kubeadm reset
-sudo apt-get purge kubeadm kubectl kubelet kubernetes-cni kube* -y
+sudo apt-get purge kubeadm kubectl kubelet kubernetes-cni kube* -y --allow-change-held-packages
 rm -rf ~/.kube
 rm -rf /etc/cni /etc/kubernetes rm -f /etc/apparmor.d/docker /etc/systemd/system/etcd*
 rm -rf /var/lib/dockershim /var/lib/etcd /var/lib/kubelet /var/lib/etcd2/ /var/run/kubernetes
@@ -12,6 +12,6 @@ iptables -t mangle -F && iptables -t mangle -X
 docker image prune -a
 systemctl restart docker
 sudo apt purge docker-engine docker docker.io docker-ce docker-ce-cli containerd containerd.io runc --allow-change-held-packages -y
-sudo apt autoremove --purge -y
+sudo apt autoremove --purge -y --allow-change-held-packages
 sudo groupdel docker
 sudo systemctl reload-daemon
