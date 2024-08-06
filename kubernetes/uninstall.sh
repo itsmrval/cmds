c3891679a29c1e9cc60039e2ad6b18e4f2bd9f59
Valentin <43043885+itsmrval@users.noreply.github.com>
2024-08-06 13:16:53 +0200

    Create uninstall.sh

---
+++ kubernetes/uninstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+kubeadm reset
+sudo apt-get purge kubeadm kubectl kubelet kubernetes-cni kube* -y
+rm -rf ~/.kube
+rm -rf /etc/cni /etc/kubernetes rm -f /etc/apparmor.d/docker /etc/systemd/system/etcd*
+rm -rf /var/lib/dockershim /var/lib/etcd /var/lib/kubelet /var/lib/etcd2/ /var/run/kubernetes
+iptables -F && iptables -X
+iptables -t nat -F && iptables -t nat -X
+iptables -t raw -F && iptables -t raw -X
+iptables -t mangle -F && iptables -t mangle -X
+docker image prune -a
+systemctl restart docker
+sudo apt purge docker-engine docker docker.io docker-ce docker-ce-cli containerd containerd.io runc --allow-change-held-packages -y
+sudo apt autoremove --purge -y
+sudo groupdel docker
+sudo systemctl reload-daemon
